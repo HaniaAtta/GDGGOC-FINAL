cfcf0a31e084b7b2ab0f703ef3e10dc6e7ae9246
Hania Atta <149865201+HaniaAtta@users.noreply.github.com>
2024-12-02 12:43:00 -0800

    update

--- my-landing-page/tailwind.config.js
+++ my-landing-page/tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
